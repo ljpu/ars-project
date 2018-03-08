272c906b907a115e2394381c6b60928652ba19e5
ljpu <leopu2nd@gmail.com>
2018-03-07 21:00:33 -0500

    remove RserveProject

--- RserveProject/src/gov/cbsa/ars/middle/service/ProcessStatus.java
+++
@@ -1,7 +0,0 @@
-package gov.cbsa.ars.middle.service;
-
-public interface ProcessStatus {
-	public enum style {running, ready, pending, failed}
-	int getProcessID();
-	void putProcessID(int processID);
-}
